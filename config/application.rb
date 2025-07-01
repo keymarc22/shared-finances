b6416ccbc2786264f2a17956da0f35dbc7a15d40
Keymar <cloraltkeymar22@gmail.com>
2025-06-30 21:42:48 -0400

    Add queue (weird)

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module CoupleFinances
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "example.com", "localhost:3000"
+        origins "http://localhost:3000"
         resource "*",
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
