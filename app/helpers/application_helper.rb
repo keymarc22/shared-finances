2323110b48167323068aad7649b5115bf97c969a
Keymar <cloraltkeymar22@gmail.com>
2025-07-05 00:01:36 -0400

    Use fixed expenses for creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,7 @@ module ApplicationHelper
   end
 
   def select_classes
-    "mt-1 h-10 w-full rounded-md border border-gray-300 bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 select"
+    "mt-1 h-10 w-full rounded-md border border-gray-300 bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 select"
   end
 
   def render_turbo_stream_flash_messages(layout = "layouts/alerts")
--- app/javascript/controllers/expense_form_controller.js
+++ app/javascript/controllers/expense_form_controller.js
@@ -2,10 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="expense-form"
 export default class extends Controller {
-  static targets = ["percentage"]
+  static targets = [
+    "percentage",
+    "description",
+    "amount",
+    "date",
+    "budget"
+  ]
 
   connect() {
     console.log("ExpenseFormController connected")
+  }
+
+  clearFields(event) {
+    event && event.preventDefault();
+    this.descriptionTarget.value = '';
+    this.amountTarget.value = '';
+    this.dateTarget.value = '';
+    this.budgetTarget.value = '';
+  }
+
+  fillFields(event) {
+    const expenseId = event.target.value
+    if (!expenseId) return this.clearFields();
+
+    const data = event.currentTarget.querySelector(`option[value="${expenseId}"]`).dataset
+    if (!data) return
+
+    this.descriptionTarget.value = data.description;
+    this.amountTarget.value = data.amount;
+    this.budgetTarget.value = data.budget;
+
+    if (data.date) {
+      this.dateTarget.value = data.date;
+    }
   }
 
   getSplitsFields(ev) {
