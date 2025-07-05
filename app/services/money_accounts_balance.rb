4deeb4bc93693f205f5e1acaa8883aee53cea2bb
Keymar <cloraltkeymar22@gmail.com>
2025-07-05 00:34:40 -0400

    Fix bug with sticky in transaction#show and add missing file MoneyAccountsBalance

---
+++ app/services/money_accounts_balance.rb
@@ -0,0 +1,17 @@
+class MoneyAccountsBalance
+  def initialize(account)
+    @account = account
+  end
+
+  def id
+    nil
+  end
+
+  def name
+    'Balance total'
+  end
+
+  def balance
+    @account.money_accounts_balance
+  end
+end
