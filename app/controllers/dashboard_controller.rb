85777a13f126765b78a9cd241c5cb0485a5ddf34
Keymar I Cloralt R <60714375+keymarc22@users.noreply.github.com>
2025-07-22 22:53:03 -0400

    Filter expenses (#32)
    * remove justify-between in expense
    
    * Use table component of shadcn
    
    * Add filter of expenses by user, budget or account
    
    * clean dashboard views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,27 +1,33 @@
 class DashboardController < ApplicationController
+  before_action :find_expenses
 
   def index
     @summary = DashboardSummaryService.new(current_account).call
-    @recent_expenses = recent_expenses_data
     @savings_plans = []
     @budgets = current_account.budgets.includes(:user)
-
-    params[:tab_content] ||= "tab_expenses"
-
-    if params[:tab_content] == "tab_user" && params[:user_id].present?
-      @user = User.find(params[:user_id])
-      @user_expenses = @user.expenses.includes(:budget, :money_account, :expense_splits)
-    end
-  end 
+    @money_accounts = current_account.money_accounts
+  end
 
   private
 
-  def recent_expenses_data
-    current_account
+  def find_expenses
+    params[:filter] ||= "recent"
+
+    scope = current_account
       .expenses
       .no_fixed
       .includes(:budget, :user, :expense_splits)
       .created_between(current_date_rage.first, current_date_rage.last)
-      .order(transaction_date: :desc, created_at: :desc)
+
+    @expenses = case params[:filter]
+    when "recent"
+      scope
+    when /\Aby_user:(\d+)\z/
+      scope.where(user_id: $1)
+    when /\Aby_budget:(\d+)\z/
+      scope.where(budget_id: $1)
+    when /\Aby_money_account:(\d+)\z/
+      scope.where(money_account_id: $1)
+    end.order(transaction_date: :desc, created_at: :desc)
   end
 end
